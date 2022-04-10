433e7c5285447af54bd3e9f4de19fe5155dea41e
Thuc <thucnguyen190702@gmail.com>
2022-04-10 14:47:25 +0700

    321

--- controllers/api.user.controller.js
+++ controllers/api.user.controller.js
@@ -7,7 +7,7 @@ exports.postReg = async (req, res, next) => {
         const salt = await bcrypt.genSalt(10);
         const user = new User(req.body);
         console.log(req.body);
-       user.password = await bcrypt.hash(req.body.password,salt);
+        user.password = await bcrypt.hash(req.body.password, salt);
 
         await user.save();
         const token = await user.generateAuthToken();
@@ -20,18 +20,41 @@ exports.postReg = async (req, res, next) => {
 //post login
 exports.postLogin = async (req, res, next) => {
     try {
-        const user = await User.findByCredentials(req.body.username,req.body.password);
-        if (!user){
-            return res.status(401).send({error:'Login failed ! Check authentication credentials'})
+        const user = await User.findByCredentials(req.body.username, req.body.password);
+        if (!user) {
+            return res.status(401).send({error: 'Login failed ! Check authentication credentials'})
         }
         const token = await user.generateAuthToken();
-        res.status(200).send({user,token});
-    }catch (error){
+        res.status(200).send({user, token});
+    } catch (error) {
         console.log(error);
         res.status(400).send(error);
     }
 }
 //get profile
-exports.getProfile = (req,res,next)=>{
+exports.getProfile = (req, res, next) => {
     res.send(req.user)
+}
+//post logout one device
+exports.postLogOut = async (req, res, next) => {
+    try {
+        req.user = req.user.tokens.filter((token) => {
+            return token.token != req.token;
+        })
+        await req.user.save();
+        res.send();
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
+
+//post logout all device
+exports.postLogOutAll = async (req, res, next) => {
+    try {
+        req.user.tokens.splice(0, user.tokens.length);
+        await req.user.save();
+        res.send();
+    } catch (error) {
+        res.status(500).send(error);
+    }
 }
--- routes/api.user.js
+++ routes/api.user.js
@@ -8,4 +8,8 @@ router.post('/user/reg',apiUser.postReg);
 router.post('/user/login',apiUser.postLogin);
 
 router.get('/user/profile',auth,apiUser.getProfile);
+
+router.post('/user/logout',auth,apiUser.postLogOut);
+
+router.post('/user/logout-all',auth,apiUser.postLogOutAll);
 module.exports = router;
